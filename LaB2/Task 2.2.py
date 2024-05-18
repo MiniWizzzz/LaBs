4834bb328ac026cb02a943efbce81811e7523218
MiniWizzzz <161350063+MiniWizzzz@users.noreply.github.com>
2024-05-18 15:13:10 +0300

    Add files via upload

---
+++ LaB2/Task 2.2.py
@@ -0,0 +1,17 @@
+
+number = int(input("Введите номер посадочного места от 1 до 54: "))
+
+if (number >= 1) and (number <= 36):
+    a = "купе,"
+elif (number >= 37) and (number <= 54):
+    a = "плацкарта,"
+
+if number % 2 == 0:
+    b = "верхняя полка"
+else:
+    b = "нижняя полка"
+
+if (number >= 1) and (number <= 54):
+    print("Ваше место", number, ":", a, b)
+else:
+    print("Не верно введен номер посадочного места")
