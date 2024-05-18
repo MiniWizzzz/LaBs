baeb073887468dce8f79a4d3d4c9017f6d94c745
MiniWizzzz <161350063+MiniWizzzz@users.noreply.github.com>
2024-05-18 22:39:41 +0300

    Add files via upload

---
+++ LaB12/Task 12.2/Task 12.2.py
@@ -0,0 +1,61 @@
+import tkinter as tk
+from classes.restaurant import IceCreamStand
+
+from components.AddItemButton import AddItemButton
+from components.RemoveItemButton import RemoveItemButton
+
+def lab_12_2():
+    # данные о магазине
+    ice1 = IceCreamStand(
+        "BrandIce",
+        "мороженое",
+        4.32,
+        ["ваниль", "фисташка", "шоколад"],
+        "проспект Проспектов, дом не завезли",
+        10,
+        21,
+    )
+
+    app = tk.Tk()
+    app.title("Карточка магазина")  # наименование для окна
+    app.geometry("600x200")         # общий размер окна
+    for c in range(3):              # колонки
+        app.columnconfigure(index=c, weight=4)
+        app.rowconfigure(index=c, weight=4)
+
+    # фрейм для информации о магазине (слева)
+    frame = tk.Frame(app, bg="MintCream")
+    frame.pack(side="left", padx=4, pady=4, fill="both", expand=True)
+
+    # стиль и расположение названия магазина
+    text_brand = tk.Label(frame, text=ice1.name, font=("Consolas", 40, "bold"),fg="teal",bg="MintCream")
+    text_brand.pack(side="top", anchor="nw")
+    # стиль и расположение рейтинга магазина
+    text_rate = tk.Label(frame, text=f"Рейтинг: {ice1.rate}", font=("Consolas", 16), bg="turquoise")
+    text_rate.pack(side="top", anchor="nw")
+    # стиль и расположение часов работы магазина
+    text_time = tk.Label(frame,text=f"Время работы: {ice1.work_start_at}-{ice1.work_end_at}",font=("Consolas", 12),fg="teal",bg="MintCream")
+    text_time.pack(side="bottom", anchor="sw")
+    # стиль и расположение местонахождения магазина
+    text_location = tk.Label(frame,text=f"Место: {ice1.location}",font=("Consolas", 12),fg="teal",bg="MintCream")
+    text_location.pack(side="bottom", anchor="sw")
+
+    # фрейм для информации о вкусах мороженого (справа)
+    frame_flavors = tk.Frame(app, bg="MintCream")
+    frame_flavors.pack(side="right", padx=4)
+
+    # вывод списка со вкусами мороженого
+    flavors_list = tk.Listbox(frame_flavors)
+    for i, fl in enumerate(ice1.flavors):
+        flavors_list.insert(i, fl)
+    flavors_list.pack(side="top")
+
+    # кнопки добавить/убрать
+    btn_add = AddItemButton(parent=frame_flavors, root=app, add_to=flavors_list)
+    btn_add.pack(side="left", fill="x", expand=True)
+    btn_remove = RemoveItemButton(frame_flavors, flavors_list)
+    btn_remove.pack(side="right", fill="x", expand=True)
+
+    app.mainloop()
+
+lab_12_2()
---
+++ LaB12/Task 12.2/classes/restaurant.py
@@ -0,0 +1,61 @@
+class Restaurant:
+
+    def __init__(self, name: str, cuisin_type: str, rate: float) -> None:
+        self.name = name
+        self.cuisin_type = cuisin_type
+        self.rate = rate
+
+    def describe_restaurant(self):
+        print(f"Название: {self.name} | Кухня: {self.cuisin_type} | Рейтинг: {self.rate}")
+
+    def open_restaurant(self):
+        print("Ресторан открыт!")
+
+    def update_rate(self, new_rate: float):
+        self.rate = new_rate
+        print(f"Рейтинг {self.name} обновлен!")
+
+
+# lab_12_1 lab 12_2
+class IceCreamStand(Restaurant):
+    # Все, что касается морожки
+
+    def __init__(
+        self,
+        name: str,
+        cuisin_type: str,
+        rate: float,
+        flavors: list[str],
+        location: str,
+        work_start: int,
+        work_end: int,
+    ) -> None:
+        super().__init__(name, cuisin_type, rate)
+        self.flavors = flavors
+        self.location = location
+        self.work_start_at = work_start
+        self.work_end_at = work_end
+
+    def get_flavors(self):
+        print("Вкусы мороженого:", ", ".join(self.flavors))
+
+    def is_exist_flavor(self, name):
+        # Есть ли такой вкус
+        return name in self.flavors
+
+    def yes_or_no_flavor(self, fl: str):
+        # для добавления вкуса
+        if self.yes_or_no_flavor(fl):
+            print("Вкус уже есть в асортименте")
+        else:
+            self.flavors.append(fl)
+            print(f'Вкус "{fl}" добавлен в асортимент')
+
+    def remove_flavor(self, fl: str):
+        # для удаления вкуса (зо што)
+        if self.yes_or_no_flavor(fl):
+            self.flavors.remove(fl)
+            print(f'Вкус "{fl}" убран из асортимента')
+        else:
+            print(f"Такой вкус не входит в асортимент магазина {self.name}.")
+            print("В асортименте:", ", ".join(self.flavors))
