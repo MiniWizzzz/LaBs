2718fcc2aac2f078d64084bc5792c015bdf18951
MiniWizzzz <161350063+MiniWizzzz@users.noreply.github.com>
2024-05-18 16:22:26 +0300

    Add files via upload

---
+++ LaB8/Task 8.2.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import sys
+
+a = {'Пасха': '1.jpg', 'пасха': '1.jpg', 'Рождество': '2.jpg', 'рождество': '2.jpg', '9 Мая': '3.jpg', '9 мая': '3.jpg'}
+day = input("Открытку к какому празднику вывести: Пасха, Рождество, 9 Мая?\nПраздник: ")
+
+img = Image.open(a[day])
+#img = Image.open(elements'popug.jpg')
+img.show()
---
+++ LaB8/Task 8.3.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageDraw, ImageFont
+import sys
+
+img = Image.open('picture.JPG')
+img_crop = img.crop((0,550,1600,1183))
+img_crop.save('picture_crop.jpg')
+
+name = input("Кого поздравим? ")
+full_text = str(name)+ ", поздравляю!"
+draw_text = ImageDraw.Draw(img_crop)
+font = ImageFont.truetype('./Styrene A LC-Black.otf', size=100)
+draw_text.text(
+    (100,100),
+    full_text,
+    font=font,
+    fill=('#000000')
+    )
+img_crop.show()
