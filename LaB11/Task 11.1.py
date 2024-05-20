33a1664217bf507cb70b0718b59b6aa44eeedbec
MiniWizzzz <161350063+MiniWizzzz@users.noreply.github.com>
2024-05-20 12:58:57 +0300

    Delete LaB11 directory

--- LaB11/Task 11.1.py
+++
@@ -1,9 +0,0 @@
-from classes.restaurant import Restaurant
-
-
-def lab_11_1():
-    # Создание экземпляра класса ресторана
-    restaurant = Restaurant("Чебуречкино", "фастфуд")
-    restaurant.describe_restaurant()
-    restaurant.open_restaurant()
-lab_11_1()
